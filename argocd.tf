d359bbb59d7e0ed633d598561f515d9f69885942
Máté Eckl <ecklm94@gmail.com>
2026-09-12 12:21:13 +0200

    Add ArgoCD install

---
+++ argocd.tf
@@ -0,0 +1,28 @@
+resource "helm_release" "argocd" {
+  name             = "argocd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  version          = "10.1.1"
+  namespace        = "argocd"
+  create_namespace = true
+
+  values = concat(
+    [
+      <<EOT
+      configs:
+        params:
+          server.insecure: true
+      server:
+        ingress:
+          enabled: true
+          tls: true
+          annotations:
+            cert-manager.io/cluster-issuer: letsencrypt
+      global:
+        domain: argocd.${local.project_domain}
+      EOT
+    ]
+  )
+
+  depends_on = [time_sleep.wait_for_kube_admin]
+}
