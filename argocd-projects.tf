7805ed4f7bd52981a3de2fbe5007d1ba15335c07
Máté Eckl <ecklm94@gmail.com>
2026-09-15 05:59:28 +0200

    fixup! feat(demo): Add projects, set autosync, set finalizer

--- argocd-projects.tf
+++ argocd-projects.tf
@@ -7,7 +7,7 @@ locals {
 
 resource "helm_release" "argocd-projects" {
   provider = helm.sbx
-  for_each = local.argocd_projects  # Just to avoid code duplication
+  for_each = local.argocd_projects # Just to avoid code duplication
 
   name       = "argocd-project-${each.key}"
   repository = "https://bedag.github.io/helm-charts"
